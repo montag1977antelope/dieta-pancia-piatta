604bd4f95ca4218d434c007e52864ba84c67074a
montag1977antelope <montag.1977@gmail.com>
2025-09-10 14:43:24 +0200

    Update weekly-planner.js

--- js/weekly-planner.js
+++ js/weekly-planner.js
@@ -640,3 +640,16 @@ function getFoodById(foodId) {
     }
     return null;
 }
+
+// Funzione debounce locale se non disponibile da utils
+function debounce(func, wait) {
+    let timeout;
+    return function executedFunction(...args) {
+        const later = () => {
+            clearTimeout(timeout);
+            func(...args);
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    };
+}
